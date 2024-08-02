79405f1e3c8652cde816486630e2d403d17fc5c6
shagufta28 <saifishagufta82@gmail.com>
2024-08-03 00:31:50 +0530

    Ready to deploy

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const carsRoutes = require('./routes/cars');
+const authRoutes = require('./routes/auth');
+const router = express.Router();
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors({
+   origin: 'http://localhost:3001'
+}));
+app.use(bodyParser.json());
+
+// Routes
+router.get('/', (req,res)=>{
+    res.send("running")
+});
+app.use('/cars', carsRoutes);
+app.use('/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Failed to connect to MongoDB Atlas', err));
+
+
+// Start the server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
